0b38ca0486f8ba6a9340b03783fc9b70878193a6
Rahul Shrivastava <rahul3103@gmail.com>
2019-06-26 20:07:10 +0530

    Updates

---
+++ precache-manifest.3a5fc30c2b3c1a39c02508fe31313b90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2cd7425e08ef701bb0a16e8991e24fe",
+    "url": "/test_header/index.html"
+  },
+  {
+    "revision": "3558278004e9b2b4505d",
+    "url": "/test_header/static/css/main.25e010a4.chunk.css"
+  },
+  {
+    "revision": "9256aa05878bf74c08d4",
+    "url": "/test_header/static/js/2.39b98acf.chunk.js"
+  },
+  {
+    "revision": "3558278004e9b2b4505d",
+    "url": "/test_header/static/js/main.09a22d80.chunk.js"
+  },
+  {
+    "revision": "493e66d5e7786eeaf6c7",
+    "url": "/test_header/static/js/runtime~main.8462c4ba.js"
+  }
+]);
