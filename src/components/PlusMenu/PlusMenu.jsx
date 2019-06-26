0ed559eebfebafb72c8210768537da98572e68ef
Rahul Shrivastava <rahul3103@gmail.com>
2019-06-26 20:17:17 +0530

    readme updaed

--- src/components/PlusMenu/PlusMenu.jsx
+++ src/components/PlusMenu/PlusMenu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MenuItem, MenuList } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Teams from '../../Club_data';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -45,140 +46,6 @@ const menuItems = {
     'Compare',
     'Free Agents',
     'Retired Players'
-  ],
-  NFL_TEAMS: [
-    {
-      key: 'AFC EAST',
-      values: [
-        {
-          name: 'Buffalo Bills',
-          logo: '149/full/n0fd1z6xmhigb0eej3323ebwq.png'
-        },
-        {
-          name: 'Miami Dolphins',
-          logo: '150/full/7306_miami_dolphins-primary-2018.png'
-        },
-        {
-          name: 'New England Patriots',
-          logo: '151/full/y71myf8mlwlk8lbgagh3fd5e0.png'
-        },
-        {
-          name: 'New York Jets',
-          logo: '152/full/9116_new_york_jets-primary-2019.png'
-        }
-      ]
-    },
-    {
-      key: 'AFC WEST',
-      values: [
-        {
-          name: 'Denver Broncos',
-          logo: '161/full/9ebzja2zfeigaziee8y605aqp.png'
-        },
-        {
-          name: 'Kansas City Chiefs',
-          logo: '162/full/857.png'
-        },
-        {
-          name: 'Los Angeles Chargers',
-          logo: '6446/full/2415_los_angeles__chargers-primary-2017.png'
-        },
-        {
-          name: 'Oakland Riders',
-          logo: '163/full/g9mgk6x3ge26t44cccm9oq1vl.png'
-        }
-      ]
-    },
-    {
-      key: 'AFC NORTH',
-      values: [
-        { name: 'Baltimore Ravens', logo: '153/full/318.png' },
-        { name: 'Cincinnati Bengals', logo: '154/full/403.png' },
-        {
-          name: 'Cleveland Browns',
-          logo: '155/full/7855_cleveland_browns-primary-2015.png'
-        },
-        { name: 'Pittsburgh Steelers', logo: '156/full/970.png' }
-      ]
-    },
-    {
-      key: 'AFC SOUTH',
-      values: [
-        { name: 'Houston Texasns', logo: '157/full/570.png' },
-        { name: 'Indianapolis Colts', logo: '158/full/593.png' },
-        {
-          name: 'Jacksonville Jaguars',
-          logo: '159/full/8856_jacksonville_jaguars-alternate-2013.png'
-        },
-        { name: 'Tennessee Titans', logo: '160/full/1053.png' }
-      ]
-    },
-    {
-      key: 'NFC EAST',
-      values: [
-        { name: 'Dallas Cowboys', logo: '165/full/406.png' },
-        { name: 'New York Giants', logo: '166/full/919.gif' },
-        { name: 'Philadelphia Eagles', logo: '167/full/960.png' },
-        {
-          name: 'Washington Redskins',
-          logo: '168/full/im5xz2q9bjbg44xep08bf5czq.png'
-        }
-      ]
-    },
-    {
-      key: 'NFC WEST',
-      values: [
-        {
-          name: 'Arizona Cardinals',
-          logo: '177/full/kwth8f1cfa2sch5xhjjfaof90.png'
-        },
-        {
-          name: 'Los Angeles Rams',
-          logo: '5941/full/7953_los_angeles_rams-primary-2017.png'
-        },
-        {
-          name: 'San Francisco 49ers',
-          logo: '179/full/9455_san_francisco_49ers-primary-2009.png'
-        },
-        {
-          name: 'Seattle Seahawks',
-          logo: '180/full/pfiobtreaq7j0pzvadktsc6jv.png'
-        }
-      ]
-    },
-    {
-      key: 'NFC NORTH',
-      values: [
-        { name: 'Chicago Bears', logo: '169/full/364.png' },
-        {
-          name: 'Detroit Lions',
-          logo: '170/full/1398_detroit_lions-primary-2017.png'
-        },
-        {
-          name: 'Green Bay Packers',
-          logo: '171/full/dcy03myfhffbki5d7il3.png'
-        },
-        {
-          name: 'Minnesota Vikings',
-          logo: '172/full/2704_minnesota_vikings-primary-2013.png'
-        }
-      ]
-    },
-    {
-      key: 'NFC SOUTH',
-      values: [
-        { name: 'Atlanta Falcons', logo: '173/full/299.png' },
-        {
-          name: 'Carolina Panthers',
-          logo: '174/full/f1wggq2k8ql88fe33jzhw641u.png'
-        },
-        { name: 'New Orleans Saints', logo: '175/full/907.png' },
-        {
-          name: 'Tampa Bay Buccaneers',
-          logo: '176/full/3670_tampa_bay_buccaneers-primary-2014.png'
-        }
-      ]
-    }
   ]
 };
 
@@ -190,7 +57,7 @@ export default function PlusMenu(props) {
     if (type !== 'NFL') return;
     return (
       <div className={classes.group}>
-        {menuItems['NFL_TEAMS'].map(teams => (
+        {Teams.NFL_TEAMS.map(teams => (
           <div className={classes.teamGroup} key={teams.key}>
             <h4>{teams.key}</h4>
             {teams.values.map(team => (
